c7c4c595b438ab012dff595e687f98407ba7eeca
njokuifeanyigerald <brainboyrichmond@gmail.com>
2022-05-23 07:46:02 +0100

    HEROKU

--- drf_urlshortener/settings.py
+++ drf_urlshortener/settings.py
@@ -10,9 +10,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
+import environ
+environ.Env.read_env()
+# I replaced the secret key line with this
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
